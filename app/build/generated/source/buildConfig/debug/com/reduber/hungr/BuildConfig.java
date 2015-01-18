e900378d3a5d3a4d51d74e47caada16cf4b76be6
Joshua Tang <jlt81@case.edu>
2015-01-18 00:32:15 -0500

    fixed a bunch of stuff login works

--- app/build/generated/source/buildConfig/debug/com/reduber/hungr/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/reduber/hungr/BuildConfig.java
@@ -5,7 +5,7 @@ package com.reduber.hungr;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.reduber.hungr";
+  public static final String PACKAGE_NAME = "com.example.hungr";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
--- app/build/generated/source/buildConfig/test/debug/com/example/hungr/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/test/debug/com/example/hungr/test/BuildConfig.java
@@ -1,11 +1,11 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.reduber.hungr.test;
+package com.example.hungr.test;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.reduber.hungr.test";
+  public static final String PACKAGE_NAME = "com.example.hungr.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = -1;
